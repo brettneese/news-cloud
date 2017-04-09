2ba8013cf89a6cd53c3b01930298fae1b115c292
Brett Neese <brett@neese.rocks>
2017-04-08 23:37:11 -0500

    date fixes

--- server/index.js
+++ server/index.js
@@ -38,13 +38,12 @@ exports.queryBigQuery = function queryBigQuery (req, res) {
     }
 
     var d = new Date()
-
-    if(req.query.sqlDate){
-      d.setDate(req.query.sqlDate);
+    if(req.body.date){
+      d = new Date(req.body.date);
     }
 
-    if(req.body.sqlDate){
-      d.setDate(req.body.sqlDate)
+    if(req.query.date){
+      d = new Date(req.query.date);
     }
     
     // start looking at the partition from the day before
@@ -52,11 +51,14 @@ exports.queryBigQuery = function queryBigQuery (req, res) {
     start.setDate(d.getDate() - 1);
 
     let sourceTable = 'gdelt-bq:gdeltv2.events';
-    if(d.sqlDate < 20050220){ 
+    
+    if(d.sqlDate() < 20050220){ 
         sourceTable = 'gdelt-bq:full.events';
     }
   
     const query = 'SELECT Actor1Name as text, NumMentions as size, SOURCEURL as href FROM [' + sourceTable +  '_partitioned] WHERE Actor1Name != "" AND SQLDATE = ' +  d.sqlDate()  + ' AND _PARTITIONTIME BETWEEN TIMESTAMP(\'' + start.sqlDateHyphenated() + '\') AND TIMESTAMP(\'' + d.sqlDateHyphenated() + '\') ORDER BY NumMentions DESC LIMIT 250;'
+    
+    console.log(query);
     
     bigQuery.query(query, function(err, rows) {
       if (err) {
@@ -69,3 +71,11 @@ exports.queryBigQuery = function queryBigQuery (req, res) {
     });
 
 };
+
+// var express = require('express')
+// var app = express()
+
+// app.get('/', this.queryBigQuery)
+// app.listen(3000, function () {
+//   console.log('Example app listening on port 3000!')
+// })
