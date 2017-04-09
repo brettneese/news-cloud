c781b46e44ec842a47ecdeef4ff6faa1c17e996f
Brett Neese <brett@neese.rocks>
2017-04-08 21:10:46 -0500

    maybe query isn't an acceptable function name?

--- server/index.js
+++ server/index.js
@@ -21,18 +21,7 @@ Date.prototype.sqlDate = function() {
 };
 
 exports.queryBigQuery = function queryBigQuery (req, res) {
-  if (req.body.message === undefined) {
-    // This is an error case, as "message" is required
-    res.status(400).send('No message defined!');
-  } else {
-    // Everything is ok
-    console.log(req.body.message);
-    res.status(200).end();
-  }
-};
-
-exports.query = function query(req, res) {
-
+  
   const d = new Date();
   const sqldate = req.query.sqldate || d.sqlDate();
 
